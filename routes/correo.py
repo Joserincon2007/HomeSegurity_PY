e75b5085fba3ee7cb7b3186ffac751919e1990fe
Joserincon2007 <joserinconxc2008@gmail.com>
2026-06-23 03:04:01 +0000

    Configuración de Flask-Mail con SMTP de Brevo para envíos en la nube

--- routes/correo.py
+++ routes/correo.py
@@ -1,13 +1,14 @@
 from flask_mail import Message
 from extensions import mail
-
+import os  
 
 def enviar_correo_credenciales(correo, password):
-
     try:
+        remitente = os.environ.get('afa7a5001@smtp-brevo.com', 'joserinconxc2008@gmail.com')
+
         msg = Message(
-            subject="✅ Credenciales de Administrador - Home Security",
-            sender="joserinconxc2008@gmail.com",
+            subject="✅ Credenciales de Autorizadas - Home Security",
+            sender=remitente,
             recipients=[correo]
         )
 
@@ -24,13 +25,13 @@ Correo: {correo}
 Contraseña: {password}
 
 Ingresa aquí:
-http://localhost:5000
+http://142.93.249.184
 
 Home Security
         """
 
         mail.send(msg)
-        print("✅ Correo enviado correctamente")
+        print(f"✅ Correo enviado correctamente a {correo} a través de Brevo")
 
     except Exception as e:
         print("❌ Error enviando correo:", e)
